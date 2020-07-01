4946bb0f391a62529e238c5cfddb0ea7604c21bb
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-01 22:59:22 +0900

    calculate total duration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+const ignoreMinTime = 7 * 60 * 60 * 1000
+
 async function run(): Promise<void> {
   try {
     const client = github.getOctokit(core.getInput('github-token'))
@@ -16,6 +18,20 @@ async function run(): Promise<void> {
       repo: context.repo.repo,
       pull_number: context.issue.number
     })
+    const dates =
+      commits.data
+        .map(commit => new Date(commit.commit.committer.date).getTime())
+    let prevDate = null
+    let totalDuration = 0
+    for (let date of dates) {
+      if (prevDate != null) {
+        const duration = date - prevDate
+        if (duration < ignoreMinTime) {
+          totalDuration = duration
+        }
+      }
+      prevDate = date
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
