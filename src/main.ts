8ecafd029e310b0302796c45a6e3308cc3142809
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-01 21:43:14 +0900

    fetch commits

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ async function run(): Promise<void> {
   try {
     const client = github.getOctokit(core.getInput('github-token'))
     const context = github.context
+    if (context.payload.action !== 'opened') {
+      return
+    }
+    if (!context.payload.pull_request) {
+      return
+    }
+    const commits = await client.pulls.listCommits({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.issue.number
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
