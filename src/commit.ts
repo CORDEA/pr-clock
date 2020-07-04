0146ec4b872456e1a464d3405efb1fff8f23c603
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-04 12:40:10 +0900

    build body

--- src/commit.ts
+++ src/commit.ts
@@ -2,8 +2,10 @@ import * as types from '@octokit/types'
 
 export class Commit {
   createdAt: Date
+  sha: string
 
   constructor(commit: types.PullsListCommitsResponseData[0]) {
     this.createdAt = new Date(commit.commit.committer.date)
+    this.sha = commit.sha
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {Commit} from './commit'
 
 const ignoreMinTime = 7 * 60 * 60 * 1000
+const title = 'Elapsed time: '
 
 async function run(): Promise<void> {
   try {
@@ -13,25 +15,33 @@ async function run(): Promise<void> {
     if (!context.payload.pull_request) {
       return
     }
-    const commits = await client.pulls.listCommits({
+    const rawCommits = await client.pulls.listCommits({
       owner: context.repo.owner,
       repo: context.repo.repo,
       // eslint-disable-next-line @typescript-eslint/camelcase
       pull_number: context.issue.number
     })
-    const dates = commits.data.map(commit =>
-      new Date(commit.commit.committer.date).getTime()
-    )
+    const commits = rawCommits.data.map(commit => new Commit(commit))
+    const waypoints = []
     let prevDate = null
     let totalDuration = 0
-    for (const date of dates) {
+    let lastIndex = commits.length - 1
+    for (let i = 0; i <= lastIndex; i++) {
+      const commit = commits[i]
+      if (i === 0 || i === lastIndex) {
+        waypoints.push(commit)
+      }
       if (prevDate != null) {
-        const duration = date - prevDate
+        const duration = commit.createdAt.getTime() - prevDate.getTime()
         if (duration < ignoreMinTime) {
           totalDuration = duration
+        } else {
+          if (i !== 0 && i !== lastIndex) {
+            waypoints.push(commit)
+          }
         }
       }
-      prevDate = date
+      prevDate = commit.createdAt
     }
     const issues = await client.issues.listComments({
       owner: context.repo.owner,
@@ -44,18 +54,28 @@ async function run(): Promise<void> {
       if (issue.user.login !== 'github-actions') {
         continue
       }
-      if (issue.body.includes('')) {
+      if (issue.body.includes(title)) {
         ownIssue = issue
       }
     }
     const duration = calcRelativeDuration(totalDuration)
+
+    let body = `${title}${duration}\n\n`
+    for (let i = 0; i < waypoints.length; i++) {
+      const waypoint = waypoints[i]
+      if (i !== 0) {
+        body += ' |\n'
+      }
+      body += `\`${waypoint.sha}\` ${waypoint.createdAt}\n`
+    }
+
     if (ownIssue !== null) {
       await client.issues.updateComment({
         owner: context.repo.owner,
         repo: context.repo.repo,
         // eslint-disable-next-line @typescript-eslint/camelcase
         comment_id: ownIssue.id,
-        body: duration
+        body: body
       })
       return
     }
@@ -64,7 +84,7 @@ async function run(): Promise<void> {
       repo: context.repo.repo,
       // eslint-disable-next-line @typescript-eslint/camelcase
       issue_number: context.issue.number,
-      body: duration
+      body: body
     })
   } catch (error) {
     core.setFailed(error.message)
