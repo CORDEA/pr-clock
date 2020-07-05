a9e51f0b9738be228cbdf11c1bf1b92f0aedf746
Yoshihiro Tanaka <contact@cordea.jp>
2020-07-05 18:59:23 +0900

    make more debuggable

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,10 @@ async function run(): Promise<void> {
       return
     }
     if (!context.payload.pull_request) {
+      core.debug('This is not PR')
       return
     }
+    core.debug('Starting...')
     const rawCommits = await client.pulls.listCommits({
       owner: context.repo.owner,
       repo: context.repo.repo,
@@ -21,37 +23,38 @@ async function run(): Promise<void> {
     })
     const commits = rawCommits.data.map(commit => new Commit(commit))
     const builder = new BodyBuilder(commits)
-    const issues = await client.issues.listComments({
+    const comments = await client.issues.listComments({
       owner: context.repo.owner,
       repo: context.repo.repo,
       // eslint-disable-next-line @typescript-eslint/camelcase
       issue_number: context.issue.number
     })
-    let ownIssue = null
-    for (const issue of issues.data) {
-      if (issue.user.login !== 'github-actions') {
+    core.debug(`Number of comments: ${comments.data.length}`)
+    let ownComment = null
+    for (const comment of comments.data) {
+      if (comment.user.login !== 'github-actions') {
         continue
       }
-      if (builder.isBody(issue.body)) {
-        ownIssue = issue
+      if (builder.isBody(comment.body)) {
+        ownComment = comment
       }
     }
 
     const body = builder.build()
     core.debug(`Body: ${body}`)
 
-    if (ownIssue !== null) {
-      core.debug(`Update #${ownIssue.id} issue`)
+    if (ownComment !== null) {
+      core.debug(`Update comment`)
       await client.issues.updateComment({
         owner: context.repo.owner,
         repo: context.repo.repo,
         // eslint-disable-next-line @typescript-eslint/camelcase
-        comment_id: ownIssue.id,
+        comment_id: ownComment.id,
         body
       })
       return
     }
-    core.debug('Create issue')
+    core.debug('Create comment')
     await client.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
