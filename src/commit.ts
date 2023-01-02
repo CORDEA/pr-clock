b14afa468003e836d5bd80d410d519649bd732fd
Yoshihiro Tanaka <contact@cordea.jp>
2023-01-02 19:14:55 +0900

    resolve error

--- src/commit.ts
+++ src/commit.ts
@@ -1,10 +1,8 @@
-import * as types from '@octokit/types'
-
 export class Commit {
   createdAt: Date
   sha: string
 
-  constructor(commit: types.PullsListCommitsResponseData[0]) {
+  constructor(commit: any) {
     this.createdAt = new Date(commit.commit.committer.date)
     this.sha = commit.sha
   }
--- src/main.ts
+++ src/main.ts
@@ -21,27 +21,25 @@ async function run(): Promise<void> {
       return
     }
     core.debug('Starting...')
-    const rawCommits = await client.pulls.listCommits({
+    const rawCommits = await client.rest.pulls.listCommits({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      // eslint-disable-next-line @typescript-eslint/camelcase
       pull_number: context.issue.number
     })
     const commits = rawCommits.data.map(commit => new Commit(commit))
     const builder = new BodyBuilder(commits)
-    const comments = await client.issues.listComments({
+    const comments = await client.rest.issues.listComments({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      // eslint-disable-next-line @typescript-eslint/camelcase
       issue_number: context.issue.number
     })
     core.debug(`Number of comments: ${comments.data.length}`)
     let ownComment = null
     for (const comment of comments.data) {
-      if (comment.user.login !== 'github-actions') {
+      if (comment?.user?.login !== 'github-actions') {
         continue
       }
-      if (builder.isBody(comment.body)) {
+      if (builder.isBody(comment.body ?? '')) {
         ownComment = comment
       }
     }
@@ -51,25 +49,25 @@ async function run(): Promise<void> {
 
     if (ownComment !== null) {
       core.debug(`Update comment`)
-      await client.issues.updateComment({
+      await client.rest.issues.updateComment({
         owner: context.repo.owner,
         repo: context.repo.repo,
-        // eslint-disable-next-line @typescript-eslint/camelcase
         comment_id: ownComment.id,
         body
       })
       return
     }
     core.debug('Create comment')
-    await client.issues.createComment({
+    await client.rest.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      // eslint-disable-next-line @typescript-eslint/camelcase
       issue_number: context.issue.number,
       body
     })
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 
